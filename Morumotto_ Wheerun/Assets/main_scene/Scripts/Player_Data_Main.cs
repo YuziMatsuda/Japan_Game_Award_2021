49ab4bcc373e251cb346bba32f0c819aea4aa08b
yuzi1112 <yuzi1112m@gmail.com>
2021-05-30 10:23:39 +0900

    全シーン格納した為、プッシュ

--- Morumotto_ Wheerun/Assets/main_scene/Scripts/Player_Data_Main.cs
+++ Morumotto_ Wheerun/Assets/main_scene/Scripts/Player_Data_Main.cs
@@ -11,24 +11,7 @@ public class Player_Data_Main : MonoBehaviour
 {
     public string datapath;
 
-<<<<<<< HEAD:Morumotto_ Wheerun/Assets/main_scene/Scripts/Player_Data_Main.cs
     [SerializeField] public bool[] stage_clear_number;   // ã‚¯ãƒªã‚¢å–å¾—
-=======
-    [SerializeField] public bool[] stage_clear_number;                      // ƒNƒŠƒAæ“¾
-    [SerializeField] public int max_stage;
-    [SerializeField] public bool delete_start;                              // ƒf[ƒ^íœŠJn
-
-    [SerializeField] private GameObject[] clear = new GameObject[5];        // ƒNƒŠƒA[
-    [SerializeField] private GameObject[] clear_prefab = new GameObject[5]; // ƒNƒŠƒA[—p‚ÌƒvƒŒƒnƒu
-    [SerializeField] private GameObject canvas;                             // ƒLƒƒƒ“ƒoƒX
-
-    [SerializeField] private float criteria_log_pos_x;                      // ƒNƒŠƒAƒƒS‚ÆÔ˜g‚ğ•\¦‚·‚éŠî€‚ÌX²
-    [SerializeField] private float move_log_pos_x;                          // ƒNƒŠƒAƒƒS‚ÆÔ˜g‚ğ•\¦‚·‚éÛ‚ÌŠÔŠu
-    [SerializeField] private float log_pos_y;                               // ƒNƒŠƒAƒƒS‚ÆÔ˜g‚ğ•\¦‚·‚éY²
-
-    private Player player;
-    private Player_Data player_data;
->>>>>>> programmer/title_sence:Morumotto_ Wheerun_Title/Assets/Scripts/All/Player_Data.cs
 
     public void Save_Data(Player_Data_Main player_data)
     {
@@ -46,12 +29,10 @@ public class Player_Data_Main : MonoBehaviour
         writer.Close();
     }
 
-<<<<<<< HEAD:Morumotto_ Wheerun/Assets/main_scene/Scripts/Player_Data_Main.cs
     public void Load_Data(Player_Data_Main player_data,string datapath)
-=======
-    public void Load_Data(Player_Data player_data, string datapath)
->>>>>>> programmer/title_sence:Morumotto_ Wheerun_Title/Assets/Scripts/All/Player_Data.cs
     {
+        // ãƒ¦ãƒ¼ã‚¶ã”ã¨ã«ä¿ç®¡ã™ã‚‹ãƒ‡ã‚£ãƒ¬ã‚¯ãƒˆãƒªãŒç•°ãªã‚‹ç‚ºã€Pathã‚’å†åº¦è¨­å®š
+        datapath = Application.dataPath + "/data/data.json";
         // ãƒ‘ã‚¹ã‚’èª­ã¿è¾¼ã‚€
         StreamReader reader = new StreamReader(datapath);
         // ãƒ•ã‚¡ã‚¤ãƒ«ã‚’èª­ã¿è¾¼ã‚€
@@ -59,7 +40,7 @@ public class Player_Data_Main : MonoBehaviour
         // ãƒ•ã‚¡ã‚¤ãƒ«ã‚’é–‰ã˜ã‚‹
         reader.Close();
 
-        JsonUtility.FromJsonOverwrite(data, player_data);
+        JsonUtility.FromJsonOverwrite(data,player_data);
     }
 
     private void Awake()
@@ -80,71 +61,12 @@ public class Player_Data_Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Init();
-    }
 
-    public void Init()
-    {
-        player = GetComponent<Player>();
-        player_data = GameObject.Find("Canvas").GetComponent<Player_Data>();
-        Load_Data(player_data, player_data.datapath);
-        max_stage = 5;
-        player_data.delete_start = false;
-        if (player.sence == Player.Character_Sence.NEXT_STAGESELECT)
-        {
-            Clear_Init();
-        }
-    }
-
-    public void Clear_Init()
-    {
-        for (int i = 0; i < max_stage; i++)
-        {
-            // ƒvƒŒƒnƒu‚ğ¶¬
-            clear_prefab[i] = (GameObject)Instantiate(clear[i]);
-            // ƒLƒƒƒ“ƒoƒX“à‚Éİ’è
-            clear_prefab[i].transform.SetParent(canvas.transform, false);
-            // ƒqƒGƒ‰ƒ‹ƒL[“à‚Ì•\¦‡‚ğ•ÏX
-            clear_prefab[i].transform.SetSiblingIndex(6);
-            // // ƒNƒŠƒAğŒ‚ğŠm”F
-            if (player_data.stage_clear_number[i])
-            {
-                clear_prefab[i].transform.localPosition = new Vector2((move_log_pos_x * i) + criteria_log_pos_x, log_pos_y);
-                clear_prefab[i].SetActive(true);
-            }
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.sence == Player.Character_Sence.GAME_DATADELETE)
-        {
-            Title_DataDelete_Check();
-        }
-    }
 
-    public void Title_DataDelete_Check()
-    {
-        if (player.Data_Complete_FlgCheck())
-        {
-            if (!player_data.delete_start)
-            {
-                // ƒf[ƒ^íœˆ—
-                player_data.delete_Data(player_data.max_stage);
-                // ƒZ[ƒuˆ—
-                player_data.Save_Data(player_data);
-                player_data.delete_start = true;
-            }
-        }
-    }
-
-    public void Select_Move_DataSave()
-    {
-        // ƒZ[ƒuƒeƒXƒg
-        player_data.stage_clear_number[0] = false;
-        player_data.stage_clear_number[1] = true;
-        player_data.stage_clear_number[2] = true;
-        player_data.Save_Data(player_data);
     }
 }
