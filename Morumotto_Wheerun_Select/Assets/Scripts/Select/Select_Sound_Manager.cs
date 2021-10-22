e35c4019fe2d2faf7ed3e4638477e0570ced2d21
YuziMatsuda <yuzi1112m@gmail.com>
2021-10-23 01:01:11 +0900

    2021年10月23日：マージ作業実施 (master)

---
+++ Morumotto_Wheerun_Select/Assets/Scripts/Select/Select_Sound_Manager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select_Sound_Manager : MonoBehaviour
+{
+    [SerializeField] private AudioClip se_decision;
+    [SerializeField] private AudioClip se_cancel;
+    [SerializeField] private AudioClip se_scroll;
+    [SerializeField] private int min_scroll_number;         // 最小のスクロール値 (０固定)
+    [SerializeField] private int max_scroll_number;         // 最大のスクロール値（最大ステージ数）
+
+    private Select_Player player;
+    private AudioSource audioSource;
+    private GameObject player_Draw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_Draw = GameObject.Find("Canvas");
+        player = player_Draw.GetComponent<Select_Player>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.sence == Select_Player.Character_Sence.GAME_STAGESELECT)
+        {
+            Output_Sound();
+        }
+    }
+
+    public void Output_Sound()
+    {
+        LeftRight_Button_Sound();
+        A_Button_Sound();
+        B_Button_Sound();
+    }
+
+    public void LeftRight_Button_Sound()
+    {
+        if (Input.GetButtonDown("Left_Input") || Input.GetButtonDown("Right_Input"))
+        {
+            // 最小スクロール値(0)と最大スクロール(最大ステージ数)の間に赤枠がある場合
+            if(player.select_stage_number > min_scroll_number && player.select_stage_number < max_scroll_number)
+            {
+                audioSource.PlayOneShot(se_scroll);
+            }
+        }
+
+        if (Input.GetButtonDown("Right_Input"))
+        {
+            // 最小スクロール値(0)に赤枠がある場合
+            if (player.select_stage_number == min_scroll_number)
+            {
+                audioSource.PlayOneShot(se_scroll);
+            }
+        }
+
+        if (Input.GetButtonDown("Left_Input"))
+        {
+            // 最大スクロール(最大ステージ数)に赤枠がある場合
+            if (player.select_stage_number == max_scroll_number)
+            {
+                audioSource.PlayOneShot(se_scroll);
+            }
+        }
+    }
+
+    public void B_Button_Sound()
+    {
+        if (Input.GetButtonDown("Decision"))
+        {
+            audioSource.PlayOneShot(se_decision);
+        }
+    }
+
+    public void A_Button_Sound()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            audioSource.PlayOneShot(se_cancel);
+        }
+    }
+}
