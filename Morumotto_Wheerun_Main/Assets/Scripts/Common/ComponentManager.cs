dcf2b9ac2c94dcefbd4b238872bef99ccee0d164
YuziMatsuda <yuzi1112m@gmail.com>
2021-10-31 15:40:14 +0900

    2021年10月31日：本番最終マージ作業実施 (master)

---
+++ Morumotto_Wheerun_Main/Assets/Scripts/Common/ComponentManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Const.Component
+{
+    /// <summary>
+    /// コンポネント名を管理
+    /// </summary>
+    public static class ComponentManager
+    {
+        /// <summary>MoveVerticalWallコンポーネント</summary>
+        public static readonly string MOVE_WALLS = "MoveWalls";
+        /// <summary>CalamariStateコンポーネント</summary>
+        public static readonly string CALAMARI_STATE = "CalamariState";
+        /// <summary>回し車の制御値</summary>
+        public static readonly string MARMOT_HEALTH = "MarmotHealth";
+        /// <summary>回し車</summary>
+        public static readonly string MARMOT_WHEEL = "MarmotWheel";
+        /// <summary>キャラクターコントローラー</summary>
+        public static readonly string CHARACTER_CONTROLLER = "CharacterController";
+        /// <summary>カラマリモードの操作</summary>
+        public static readonly string CALAMARI_MOVE_CONTROLLER = "CalamariMoveController";
+        /// <summary>ネンチャクモードの操作</summary>
+        public static readonly string NENCHAK_MOVE_CONTROLLER = "NenchakMoveController";
+        /// <summary>ツルツルモードの操作</summary>
+        public static readonly string TSURUTSURU_MOVE_CONTROLLER = "TsuruTsuruMoveController";
+        /// <summary>ベルトコンベア上でプレイヤーを動かす</summary>
+        public static readonly string CONVEYOR_MOVE_CHARACTER = "ConveyorMoveCharacter";
+        /// <summary>回転リング</summary>
+        public static readonly string CIRCLE_RING = "CircleRing";
+        /// <summary>滑る床</summary>
+        public static readonly string ICE_PLANE = "IcePlane";
+    }
+}
